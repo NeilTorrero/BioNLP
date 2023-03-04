f56616142818657be5279ec4cfd1ecc2f19817f0
Neil <43568278+NeilTorrero@users.noreply.github.com>
2023-03-04 19:20:38 +0100

    Added Abbreviation check and name clean up

---
+++ Preprocessing/BioAbbr.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+import medialpy
+
+df = pd.read_csv(r'BioNLP2023-1A-Train.csv')
+#remove rows with empty GT
+df = df.dropna(subset=['Summary']).reset_index(drop=True)
+
+i_a = 0
+i_a_2 = 0
+i_s = 0
+i_s_2 = 0
+for idx in range(len(df.index)):
+    #remove image id
+    ass = str(df.loc[idx]['Assessment'])
+    sub = str(df.loc[idx]['Subjective Sections'])
+    words_a = word_tokenize(ass)
+    print(ass)
+    print(words_a)
+    input()
+    words_s = word_tokenize(sub)
+    for i, w in enumerate(words_a):
+        if medialpy.exists(w):
+            i_a += 1
+            term = medialpy.find(w)
+            print(w + ': ')
+            print(term.meaning)
+            if len(term.meaning) == 1:
+                words_a[i] = term.meaning[0]
+            else:
+                i_a_2 += 1
+    input()
+    print(sub)
+    print(words_s)
+    input()
+    for i, w in enumerate(words_s):
+        if medialpy.exists(w):
+            i_s += 1
+            term = medialpy.find(w)
+            print(w + ': ')
+            print(term.meaning)
+            if len(term.meaning) == 1:
+                words_s[i] = term.meaning[0]
+            else:
+                i_s_2 += 1
+    input()
+
+print(i_a)
+print(i_a_2)
+print(i_s)
+print(i_s_2)
+
+#df.to_csv(r'BioNLP_Abbr.csv', index=False)
