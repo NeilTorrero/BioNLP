3df52392d1716e669bbf2a726993eb4e57df59eb
Neil <43568278+NeilTorrero@users.noreply.github.com>
2023-03-27 13:53:28 +0200

    Log Prediction Label and Summary comparison

--- SumWords.py
+++ SumWords.py
@@ -7,16 +7,17 @@ import torch
 
 from transformers import AutoModelForTokenClassification, pipeline, AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("model/end", local_files_only=True)
-model = AutoModelForTokenClassification.from_pretrained("model/end", local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained("model/checkpoint-1560", local_files_only=True)
+model = AutoModelForTokenClassification.from_pretrained("model/checkpoint-1560", local_files_only=True)
 
 finetunedmodel = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy='average')
 
 res = finetunedmodel("acute exacerbation a 59 year-old man presents with afib, malaise, heart attack and hypoxia")
 #print(res)
 
-#mimic = load_dataset('csv', data_files="Preprocessing/NER/BioT2S.csv")
+mimic2 = load_dataset('csv', data_files="Preprocessing/NER/BioT2S.csv")
 mimic = load_dataset('csv', data_files="Preprocessing/BioNLP_PP_SAP.csv")
+mimic['train'] = mimic['train'].add_column('Labels', mimic2['train']['words'])
 mimic = mimic['train'].train_test_split(test_size=0.2)
 test_valid = mimic['test'].train_test_split(test_size=0.5)
 mimic = DatasetDict({
@@ -27,13 +28,13 @@ mimic = DatasetDict({
 
 
 def fix_words(ex):
-    for i, w in enumerate(ex['Words']):
-        #ex['Words'][i] = literal_eval(ex['Words'][i])
+    for i, w in enumerate(ex['Text']):
+        ex['Labels'][i] = literal_eval(ex['Labels'][i])
         ex['Summary'][i] = str(ex['Summary'][i]).lower()
     return ex
 
 mimic = mimic.map(fix_words, batched=True)
-#mimic = mimic.cast_column('words', Sequence(feature=Value(dtype='string', id=None), length=-1, id=None))
+mimic = mimic.cast_column('Labels', Sequence(feature=Value(dtype='string', id=None), length=-1, id=None))
 mimic = mimic.cast_column('Text', Value(dtype='string', id=None))
 mimic = mimic.cast_column('Summary', Value(dtype='string', id=None))
 mimic = mimic.filter(lambda example: len(example["Text"]) > 0)
@@ -41,17 +42,21 @@ mimic = mimic.filter(lambda example: len(example["Text"]) > 0)
 print(mimic)
 
 rouge = evaluate.load("rouge")
-
-for ex in mimic['train']:
+log = open("predictions.log", "w")
+for ex in mimic['test']:
     predictions = []
     references = []
     words = ""
     res = finetunedmodel(ex['Text'])
     for match in res:
         words += match['word'] + '; '
-    #words = '; '.join(list(dict.fromkeys(ex['words'])))
+    labels = '; '.join(list(dict.fromkeys(ex['Labels'])))
     predictions.append(words)
     references.append(ex['Summary'])
+    log.write('Pred= ' + words + '\n')
+    log.write('Labels= ' + labels + '\n')
+    log.write('Sum=' + ex['Summary'] + '\n')
+    log.write('\n')
     rouge.add_batch(predictions=predictions, references=references)
 
 final_score = rouge.compute()
