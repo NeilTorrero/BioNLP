0ddd8049077a32490f24e0530e20715be0271be9
Neil <43568278+NeilTorrero@users.noreply.github.com>
2023-04-16 12:27:58 +0200

    Param and no ner pretrain

--- BioBertCustom.py
+++ BioBertCustom.py
@@ -12,8 +12,6 @@ from datasets import Dataset, DatasetDict, ClassLabel, Sequence, Value, load_dat
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-bc5cdr = load_dataset("tner/bc5cdr")
-ncbi = load_dataset("ncbi_disease")
 mimic = load_dataset('csv', data_files="Resources/BioNLP2_dataset1.csv")
 mimic = mimic['train'].train_test_split(test_size=0.2)
 test_valid = mimic['test'].train_test_split(test_size=0.5)
@@ -39,31 +37,6 @@ def rework_tags(ex):
                 ex["tags"][i][j] = 0
     return ex
 
-bc5cdr = bc5cdr.map(rework_tags, batched=True)
-#bc5cdr = bc5cdr.cast_column('tags', Sequence(feature=ClassLabel(names=["O", "B-Chemical", "B-Disease", "I-Disease", "I-Chemical"], id=None), length=-1, id=None))
-bc5cdr = bc5cdr.cast_column('tags', Sequence(feature=ClassLabel(names=["O", "B-Disease", "I-Disease"], id=None), length=-1, id=None))
-bc5cdr = bc5cdr.filter(lambda example: len(example["tags"]) > 0)
-bc5cdr = bc5cdr.filter(lambda ex: 1 in ex['tags'] or 2 in ex['tags'])
-# Adapt NCBI
-ncbi = ncbi.filter(lambda example: len(example["ner_tags"]) > 0)
-ncbi = ncbi.remove_columns('id')
-ncbi = ncbi.rename_column("ner_tags", "tags")
-#ncbi = ncbi.cast_column('tags', Sequence(feature=ClassLabel(names=["O", "B-Chemical", "B-Disease", "I-Disease", "I-Chemical"], id=None), length=-1, id=None))
-ncbi = ncbi.cast_column('tags', Sequence(feature=ClassLabel(names=["O", "B-Disease", "I-Disease"], id=None), length=-1, id=None))
-
-#Adapt tags to bc5cdr (1 -> 2, 2 -> 3)
-#def change_tags(ex):
-#    for i, tags in enumerate(ex['tags']):
-#        for j, tag in enumerate(tags):
-#            if tag == 1:
-#                ex["tags"][i][j] = 2
-#            else:
-#                if tag == 2:
-#                    ex["tags"][i][j] = 3
-#    return ex
-
-#ncbi = ncbi.map(change_tags, batched=True)
-
 # Adapt MIMIC don't needed here already been done in preprocessing
 def int_tags(ex):
     for i, tags in enumerate(ex['tags']):
@@ -76,15 +49,6 @@ mimic = mimic.cast_column('tokens', Sequence(feature=Value(dtype='string', id=No
 mimic = mimic.cast_column('tags', Sequence(feature=ClassLabel(names=["O", "B-Disease", "I-Disease"], id=None), length=-1, id=None))
 mimic = mimic.filter(lambda example: len(example["tags"]) > 0)
 
-
-# Merge
-datasets = DatasetDict()
-datasets['train'] = concatenate_datasets([bc5cdr['train'],ncbi['train']])
-datasets['validation'] = concatenate_datasets([bc5cdr['validation'],ncbi['validation']])
-datasets['test'] = concatenate_datasets([bc5cdr['test'],ncbi['test']])
-print(datasets)
-
-
 labels_bio = ["O", "B-Disease", "I-Disease"]
 
 # Tokenize and adapt datasets to tokenization
@@ -92,7 +56,7 @@ labels_bio = ["O", "B-Disease", "I-Disease"]
 #tokenizer = AutoTokenizer.from_pretrained("alvaroalon2/biobert_diseases_ner")
 #tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.2")
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-example = datasets['train'][0]
+example = mimic['train'][0]
 tokenized = tokenizer(example["tokens"], is_split_into_words=True)
 tokens = tokenizer.convert_ids_to_tokens(tokenized["input_ids"])
 
@@ -115,8 +79,6 @@ def tokenize_and_realign(ex):
     tokenized_ex["labels"] = labels
     return tokenized_ex
 
-tokenized_dataset = datasets.map(tokenize_and_realign, batched=True)
-tokenized_dataset = tokenized_dataset.remove_columns(['tokens', 'tags'])
 tokenized_mimic = mimic.map(tokenize_and_realign, batched=True)
 tokenized_mimic = tokenized_mimic.remove_columns(['tokens', 'tags'])
 
@@ -159,61 +121,10 @@ model = AutoModelForTokenClassification.from_pretrained("bert-base-uncased", num
 training_args = TrainingArguments(
     output_dir="model",
     learning_rate=5e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    #gradient_accumulation_steps=2,
-    num_train_epochs=2,
-    weight_decay=0.01,
-    evaluation_strategy="steps",
-    save_strategy="steps",
-    logging_steps=50,
-    eval_steps=50,
-    load_best_model_at_end=True,
-    metric_for_best_model="loss"
-)
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_dataset["train"],
-    eval_dataset=tokenized_dataset["validation"],
-    tokenizer=tokenizer,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics
-)
-
-trainer.train()
-trainer.save_model('model/ner/')
-
-trainer.evaluate()
-
-logits, labels, _ = trainer.predict(tokenized_dataset["test"])
-predictions = np.argmax(logits, axis=-1)
-
-# Remove ignored index (special tokens)
-true_predictions = [
-    [labels_bio[p] for (p, l) in zip(prediction, label) if l != -100]#(l != -100 and l != 0)]
-    for prediction, label in zip(predictions, labels)
-]
-true_labels = [
-    [labels_bio[l] for l in label if l != -100]#(l != -100 and l != 0)]
-    for label in labels
-]
-
-results = seqeval.compute(predictions=true_predictions, references=true_labels)
-print('All datasets test')
-print(results)
-
-tokenizer = AutoTokenizer.from_pretrained('model/ner/', local_files_only=True)
-model = AutoModelForTokenClassification.from_pretrained('model/ner/', local_files_only=True)
-
-training_args = TrainingArguments(
-    output_dir="model",
-    learning_rate=5e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
     gradient_accumulation_steps=2,
-    #num_train_epochs=2,
+    num_train_epochs=2,
     weight_decay=0.01,
     evaluation_strategy="steps",
     save_strategy="steps",
