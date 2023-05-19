c69fe99e1355009d48ba2a6f5707e5c275de4dd7
Neil <43568278+NeilTorrero@users.noreply.github.com>
2023-05-19 10:29:07 +0200

    Rouge compute for Dataset

--- Preprocessing/NER/RougeDataset.py
+++ Preprocessing/NER/RougeDataset.py
@@ -18,7 +18,7 @@ import torch
 #res = finetunedmodel("acute exacerbation a 59 year-old man presents with afib, malaise, heart attack and hypoxia")
 #print(res)
 
-mimic = load_dataset('csv', data_files="Resources/Preprocessing/NER/BioT2S2.csv")
+mimic = load_dataset('csv', data_files="Preprocessing/NER/Resources/BioT2S2.csv")
 
 
 def fix_words(ex):
@@ -35,14 +35,30 @@ mimic = mimic.filter(lambda example: len(example["words"]) > 0)
 print(mimic)
 
 rouge = evaluate.load("rouge")
-import difflib
-import re
 
+from rouge_score import rouge_scorer
+
+def applyPyRouge(gts, preds):
+    scorer = rouge_scorer.RougeScorer(['rouge1','rouge2','rougeL'])
+    scores = scorer.score(gts, preds) 
+    return scores 
+
+    
+def compute_rouge(gt, pred):
+    sc = applyPyRouge(gt, pred)
+    return sc['rougeL'].precision, sc['rougeL'].recall, sc['rougeL'].fmeasure
+
+import difflib
+import re, numpy as np
+
+metrics = {}
+precs, recs, f1s = [], [], [] 
 log = open("Preprocessing/NER/Resources/dataset_rouge.log", "w")
 for idx, ex in enumerate(mimic['train']):
     predictions = []
     references = []
     rouge2 = evaluate.load("rouge")
+    #wlist = ex['words']
     wlist = list(dict.fromkeys(ex['words']))
     for i, e in enumerate(wlist):
         # fix spaces on special characters (s / p)
@@ -59,9 +75,15 @@ for idx, ex in enumerate(mimic['train']):
     log.write('Prediction:\n\t' + pred + '\n')
     log.write('Summary:\n\t' + ex['summary'] + '\n')
     log.write(str(rouge2.compute(predictions=[pred], references=[ex['summary']])))
+    log.write(str(compute_rouge(ex['summary'], pred)))
     log.write('\n')
     predictions.append(pred)
     references.append(ex['summary'])
+
+    precision, recall, f1 = compute_rouge(ex['summary'], pred)
+    precs.append(precision)
+    recs.append(recall)
+    f1s.append(f1)
     rouge.add_batch(predictions=predictions, references=references)
 
 final_score = rouge.compute()
@@ -69,3 +91,10 @@ final_score = rouge.compute()
 log.write('\n\nFinal score\n')
 log.write(str(final_score))
 print(final_score)
+
+metrics["precision"] = np.mean(precs)
+metrics["recall"] = np.mean(recs)
+metrics["f1"] = np.mean(f1s)
+print(metrics)
+log.write('\n')
+log.write(str(metrics))
